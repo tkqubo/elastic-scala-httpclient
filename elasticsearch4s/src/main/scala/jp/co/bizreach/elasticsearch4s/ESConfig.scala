2b97d7562aff699470158346036858495d253d9e
Naoki Takezoe <takezoe@gmail.com>
2014-12-25 00:27:58 +0900

    Refactor url generation

--- elasticsearch4s/src/main/scala/jp/co/bizreach/elasticsearch4s/ESConfig.scala
+++ elasticsearch4s/src/main/scala/jp/co/bizreach/elasticsearch4s/ESConfig.scala
@@ -1,6 +1,12 @@
 package jp.co.bizreach.elasticsearch4s
 
-case class ESConfig(indexName: String, typeName: Option[String] = None)
+case class ESConfig(indexName: String, typeName: Option[String] = None){
+  def url(baseUrl: String) = typeName.map { typeName =>
+    s"${baseUrl}/${indexName}/${typeName}"
+  }.getOrElse {
+    s"${baseUrl}/${indexName}"
+  }
+}
 
 object ESConfig {
   /**
