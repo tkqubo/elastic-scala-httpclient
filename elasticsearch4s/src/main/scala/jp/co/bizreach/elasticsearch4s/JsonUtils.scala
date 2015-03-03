f42995023d899c92ab9ae1ad4fc36b888f6b5891
Naoki Takezoe <takezoe@gmail.com>
2015-03-03 16:43:25 +0900

    Keep ObjectMapper instance statically

--- elasticsearch4s/src/main/scala/jp/co/bizreach/elasticsearch4s/JsonUtils.scala
+++ elasticsearch4s/src/main/scala/jp/co/bizreach/elasticsearch4s/JsonUtils.scala
@@ -10,37 +10,26 @@ import scala.reflect.ClassTag
 
 private[elasticsearch4s] object JsonUtils {
 
-  def serialize(doc: AnyRef): String = {
-    val mapper = new ObjectMapper()
-    mapper.registerModule(DefaultScalaModule)
-    val testModule = new SimpleModule("MyModule", Version.unknownVersion())
-      .addSerializer(classOf[DateTime], new JsonSerializer[DateTime] {
-        override def serialize(value: DateTime, generator: JsonGenerator, provider: SerializerProvider): Unit = {
-          val formatter = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").withZoneUTC()
-          generator.writeString(formatter.print(value))
-        }
-      })
+  val mapper = new ObjectMapper()
+  mapper.enable(DeserializationFeature.UNWRAP_SINGLE_VALUE_ARRAYS)
+  mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+  mapper.registerModule(new SimpleModule("MyModule", Version.unknownVersion())
+    .addSerializer(classOf[DateTime], new JsonSerializer[DateTime] {
+      override def serialize(value: DateTime, generator: JsonGenerator, provider: SerializerProvider): Unit = {
+        val formatter = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").withZoneUTC()
+        generator.writeString(formatter.print(value))
+      }
+    })
+    .addDeserializer(classOf[DateTime], new JsonDeserializer[DateTime](){
+      override def deserialize(parser: JsonParser, context: DeserializationContext): DateTime = {
+        val formatter = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").withZoneUTC()
+        formatter.parseDateTime(if(parser.getValueAsString != null) parser.getValueAsString else parser.nextTextValue)
+      }
+    })
+  )
 
-    mapper.registerModule(testModule)
-    mapper.writeValueAsString(doc)
-  }
+  def serialize(doc: AnyRef): String = mapper.writeValueAsString(doc)
 
-  def deserialize[T](json: String)(implicit c: ClassTag[T]): T = {
-    val mapper = new ObjectMapper()
-    mapper.registerModule(DefaultScalaModule)
-    mapper.enable(DeserializationFeature.UNWRAP_SINGLE_VALUE_ARRAYS)
-
-    val testModule = new SimpleModule("MyModule", Version.unknownVersion())
-      .addDeserializer(classOf[DateTime], new JsonDeserializer[DateTime](){
-        override def deserialize(parser: JsonParser, context: DeserializationContext): DateTime = {
-          val formatter = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").withZoneUTC()
-          formatter.parseDateTime(if(parser.getValueAsString != null) parser.getValueAsString else parser.nextTextValue)
-        }
-      })
-
-    mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-    mapper.registerModule(testModule)
-    mapper.readValue(json, c.runtimeClass).asInstanceOf[T]
-  }
+  def deserialize[T](json: String)(implicit c: ClassTag[T]): T = mapper.readValue(json, c.runtimeClass).asInstanceOf[T]
 
 }
