6d72cf1bb544d5d5d1f240e10655e08a9ea8eeb0
Naoki Takezoe <takezoe@gmail.com>
2016-10-11 18:39:55 +0900

    (refs #35)Fix testcase

--- elastic-scala-httpclient/src/test/scala/jp/co/bizreach/elasticsearch4s/ESConfigTest.scala
+++ elastic-scala-httpclient/src/test/scala/jp/co/bizreach/elasticsearch4s/ESConfigTest.scala
@@ -5,15 +5,15 @@ class ESConfigTest extends FunSuite {
 
 
   test("urlWithParameters 1") {
-    assert(ESConfig("index", preference = Some("session"), explain = (true), timeout = Some(1000)).urlWithParameters("base","path") == "base/index/path?preference=session&explain=true&timeout=1000")
+    assert(ESConfig("index", preference = Some("session"), explain = (true), timeout = Some(1000)).urlWithParameters("base","path") == "base/index/path?preference=session&explain=true&timeout=1000ms")
   }
 
   test("urlWithParameters 2") {
-    assert(ESConfig("index", preference = None, explain = (true), timeout = Some(1000)).urlWithParameters("base","path") == "base/index/path?explain=true&timeout=1000")
+    assert(ESConfig("index", preference = None, explain = (true), timeout = Some(1000)).urlWithParameters("base","path") == "base/index/path?explain=true&timeout=1000ms")
   }
 
   test("urlWithParameters 3") {
-    assert(ESConfig("index", preference = Some("preference"), explain = (false), timeout = Some(1000)).urlWithParameters("base","path") == "base/index/path?preference=preference&timeout=1000")
+    assert(ESConfig("index", preference = Some("preference"), explain = (false), timeout = Some(1000)).urlWithParameters("base","path") == "base/index/path?preference=preference&timeout=1000ms")
   }
 
   test("urlWithParameters 4") {
